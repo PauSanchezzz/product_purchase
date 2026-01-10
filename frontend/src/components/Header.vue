dfe224aedcf3e80407de724443f19d2521f8ce22
Paula Sanchez <pausanchezt12@gmail.com>
2026-01-10 10:17:36 -0500

    [add] header of the page

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { formatDate } from '@/utils/mixins'
+import 'primeicons/primeicons.css'
+</script>
+
+<template>
+  <header>
+    <div>
+      <p class="text-m--gray">{{ formatDate(new Date().toString()) }}</p>
+      <p class="text-l--bold">¡Bienvenido a Tú Tienda!</p>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  position: fixed;
+  background-color: #f5f6fa;
+  width: 100%;
+  z-index: 1;
+}
+</style>
